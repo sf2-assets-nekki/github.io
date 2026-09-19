07730c9664e5c417ea61b84a8c858aa6eaa59f37
sf2-assets-nekki <accsfvip@gmail.com>
2026-09-19 21:00:52 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$url = "https://sf2lb.nekkimobile.ru/balance";
+$queryString = $_SERVER['QUERY_STRING'] ?? '';
+if ($queryString !== '') {
+    $url .= '?' . $queryString;
+}
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+if ($response === false) {
+    http_response_code(502);
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode([
+        'error' => curl_error($ch)
+    ]);
+    exit;
+}
+header('Content-Type: application/json; charset=utf-8');
+echo $response;
